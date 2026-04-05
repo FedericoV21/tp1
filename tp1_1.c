7a252e441d1d207aaba12295f4f15fe508301f8c
ArdillaV21 <fedevalle21@gmail.com>
2026-04-05 13:40:45 -0300

    add: creo puntero y variable muestro sus propiedades

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(){
-
-    printf("Hola Mundo\n");
     
+    int *puntero;
+    int variable = 10;
+
+    puntero = &variable;
+
+    printf("El contenido del puntero es; %d\n", *puntero);
+    printf("La direccion de memoria almacenada en el puntero es: %p \n", puntero);
+    printf("La direccion de memoria de la variable es: %p \n", &variable);
+    printf("La direccion de memoria del puntero es; %p \n", &puntero);
+    printf("El tamaño de memoria utilizado por la variable es: %lu bytes \n", sizeof(variable));
     return 0;
  }
